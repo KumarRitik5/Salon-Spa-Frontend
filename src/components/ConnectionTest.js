b03515a5321707b5e97aca75c010a8a1e856122a
Ritik Kumar <ritikkumar12bicbly@gmail.com>
2025-08-27 13:15:25 +0530

    Fix import paths and simplify vercel.json

--- src/components/ConnectionTest.js
+++ src/components/ConnectionTest.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import api, { API_BASE_URL } from '../../utils/api';
+import api, { API_BASE_URL } from '../utils/api';
 
 const ConnectionTest = () => {
   const [testResult, setTestResult] = useState('');
